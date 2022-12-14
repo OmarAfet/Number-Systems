2023e334610c1be90fff1f8bfeeae543dbda99e8
M7ilan <81147447+M7ilan@users.noreply.github.com>
2022-12-14 11:44:42 +0300

    Update Decimal_To_Binary.py

--- Decimal_To_Binary.py
+++ Decimal_To_Binary.py
@@ -8,14 +8,11 @@ def Decimal_To_Binary(Decimal):
     for i in Binarys:
         result += str(i)
 
-    # Add the rest of the zeros
     while len(result) % 4 != 0:
         result = "0" + result
         
-    # Spacing every 4 digits
     result = " ".join(result[i:i+4] for i in range(0, len(result), 4))
     
     return result
 
-# Example "123" (Decimal) to "0111 1011" (Binary)
 print(Decimal_To_Binary(123))
