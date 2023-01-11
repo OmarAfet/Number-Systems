36a27162c12432d42cc0bcd7958496d3d74c9ad6
M7ilan <distny2004@gmail.com>
2023-01-11 13:35:08 +0300

    #

--- Binary (Base 2)/Add_Binary.py
+++ Binary (Base 2)/Add_Binary.py
@@ -2,8 +2,5 @@ def Add_Binary(a: str, b: str) -> str:
     x = int(a, 2)
     y = int(b, 2)
     z = bin(x + y)[2:]
-    
-    while len(z) % 4 != 0:
-        z = "0" + z
 
     return z
--- Binary (Base 2)/Sub_Binarys.py
+++ Binary (Base 2)/Sub_Binarys.py
@@ -1,9 +1,9 @@
 def Sub_Binary(a: str, b: str) -> str:
     x = int(a, 2)
     y = int(b, 2)
-    z = bin(x - y)[2:]
+    if y > x:
+        z = "-" + bin(y - x)[2:]
+    else:
+        z = bin(x - y)[2:]
     
-    while len(z) % 4 != 0:
-        z = "0" + z
-
     return z
