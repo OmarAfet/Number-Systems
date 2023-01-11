fef8c7764a63fadfe6ed348c7ea86e43314ec38e
M7ilan <distny2004@gmail.com>
2023-01-11 13:01:38 +0300

    #

--- Hexadecimal/Hexadecimal_To_Binary.py
+++ Hexadecimal/Hexadecimal_To_Binary.py
@@ -1,2 +1,8 @@
 def Hexadecimal_To_Binary(Hexadecimal: str) -> int:
-    return bin(int(Hexadecimal, 16))[2:]
+    x = bin(int(Hexadecimal, 16))[2:]
+
+    while len(x) % 4 != 0:
+        x = "0" + x
+
+    return " ".join(x[i:i+4] for i in range(0, len(x), 4))
+
--- Octal/Octal_To_Binary.py
+++ Octal/Octal_To_Binary.py
@@ -1,2 +1,7 @@
 def Octal_To_Binary(Octal: int) -> int:
-    return bin(int(str(Octal), 8))[2:]
+    x = bin(int(str(Octal), 8))[2:]
+
+    while len(x) % 4 != 0:
+        x = "0" + x
+
+    return " ".join(x[i:i+4] for i in range(0, len(x), 4))
